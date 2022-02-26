f1ae9cd77ea2b3a04efbcc89344c8360c04f369a
Lishan1212 <lishalishan12.com>
2022-02-26 15:54:33 +0530

    login UI created with facebook google logo

---
+++ login_ui/lib/config/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/painting.dart';
+
+class Palette {
+ static const Color iconColor = Color(0xFFB6C7D1);
+ static const Color activeColor = Color.fromARGB(255, 180, 19, 185);
+ static const Color textColor1 = Color(0XFFA7BCC7);
+ static const Color textColor2 = Color.fromARGB(255, 17, 19, 20);
+ static const Color facebookColor = Color.fromARGB(255, 0, 71, 224);
+ static const Color googleColor = Color.fromARGB(255, 17, 156, 114);
+ static const Color backgroundColor = Color(0xFFECF3F9);
+}
---
+++ login_ui/lib/main.dart
@@ -0,0 +1,22 @@
+
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:login_ui/pages/login_signup.dart';
+
+void main() {
+  runApp(LoginSignupUI());
+}
+
+class LoginSignupUI extends StatelessWidget {
+  const LoginSignupUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Login Signup UI",
+      home: LoginSignupScreen(),
+    );
+  }
+}
